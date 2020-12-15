b76c81eb29ad6533432c59ca0723cfa6ed2b6a8b
robbiab <robbyabd110@gmail.com>
2020-12-15 17:16:37 +0700

    stepper

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var stepper = new Stepper(document.querySelector('.bs-stepper'))
+let stepper = null;
+const nextStep = document.getElementById("next");
+const finishStep = document.getElementById("finish");
+
+document.addEventListener("DOMContentLoaded", function () {
+  var stepperEl = document.getElementById("stepper");
+  stepper = new Stepper(stepperEl);
+
+  stepperEl.addEventListener("show.bs-stepper", function (event) {
+    let indexStep = event.detail.indexStep;
+
+    if (indexStep == 1) {
+        nextStep.classList.toggle('d-none');
+        finishStep.classList.toggle('d-none');
+    }else{
+        nextStep.classList.toggle('d-none');
+        finishStep.classList.toggle('d-none');
+    }
+  });
 });
 
-const next = document.querySelector("#next");
-const prev = document.querySelector("#previous");
-
-next.addEventListener('click', (e) => {
-    var stepper = new Stepper(document.querySelector('.bs-stepper'))
-    stepper.next()
+finishStep.addEventListener("click", () => {
+    console.log('hore')
 });
 
-prev.addEventListener('click', (e) => {
-    var stepper = new Stepper(document.querySelector('.bs-stepper'))
-    stepper.previous()
-});
+/**
+ * assalamualakum pak/buk. Nama saya {nama}, dari kelas {kelas}. {urusan} {pertanyaan} {ucapanPenutup}
+ * 
+ * Assalamualaikum pak. Nama saya robbi, dari kelas Rpl 2. ingin tanya pelajar mtk di kumpulkan ke siapa ya ? oke pak terima kasih
+ */
